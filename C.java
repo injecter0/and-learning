d5b9a8ee971b055807bc92b7ddc4d3541721540a
injecter0 <52383844+injecter0@users.noreply.github.com>
2019-06-30 20:46:33 +0100

    Add files via upload

---
+++ C.java
@@ -0,0 +1,20 @@
+public class C {
+ private    int a;
+  private   int b;
+
+
+public void getNumbers(int a,int b,int c) {
+
+    this.a = a;
+    this.b = b;
+
+
+    if (a > b) {
+        System.out.println("num1 is bigger");
+    } else if (a == b) {
+        System.out.println("num1 = numb2");
+    } else {
+        System.out.println("num2>num1 " );
+    }
+
+}}
---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+public class Main {
+
+
+    public static void main(String[] args) {
+
+
+            JOptionPane.showMessageDialog(null,"ERROR FROM HOST","NO CAN DO",JOptionPane.INFORMATION_MESSAGE);
+        }
+
+    }
+
+
+
+
