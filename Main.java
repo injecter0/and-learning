6ba23f4d04d95cca17939b031d2b8805b6df1450
injecter0 <52383844+injecter0@users.noreply.github.com>
2019-07-01 01:24:47 +0100

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,16 +1,27 @@
-import java.util.Scanner;
+
 import javax.swing.JOptionPane;
+import java.util.Scanner;
+
 public class Main {
 
 
     public static void main(String[] args) {
 
+        Scanner B=new Scanner(System.in);
+System.out.println("inter the numbers of time PLS");
+        int h=B.nextInt();
+        int m=B.nextInt();
+        int s=B.nextInt();
 
-            JOptionPane.showMessageDialog(null,"ERROR FROM HOST","NO CAN DO",JOptionPane.INFORMATION_MESSAGE);
-        }
+        Clock now=new Clock();
+
+        now.setClock(h,m,s);
+
+        JOptionPane.showMessageDialog(null,"The Clock show !! : "+ now  );
+
 
     }
-
+}
 
 
 
